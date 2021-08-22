e3942a7abdd69d1e0f0c8f56473090c0d47ba983
vesord <vitalii.evstratov@gmail.com>
2021-08-23 11:32:09 +1200

    custom root, any degree, pos d solving

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,96 @@
 use std::ops;
-use std::iter;
-
+use std::collections::HashMap;
 
 #[derive(Debug)]
 struct Polynomial {
-    a: f64,
-    b: f64,
-    c: f64,
+    degrees: HashMap<i32, f64>,
 }
 
-impl iter::Sum<Polynomial> for Polynomial {
-    fn sum<I: Iterator<Item=Polynomial>>(iter: I) -> Self {
-        let mut res = Polynomial {a: 0., b: 0., c: 0.};
-        for i in iter {
-            res.a += i.a;
-            res.b += i.b;
-            res.c += i.c;
-        };
-        Polynomial {a: res.a, b: res.b, c: res.c}
+impl Polynomial {
+    fn print_reduced(&self) {
+        let mut m: Vec<(&i32, &f64)> = self.degrees.iter().collect();
+        m.sort_by(|a, b| b.0.cmp(a.0));
+        let reduced_terms: Vec<String> = m.iter().map(|kv| format!("{}*X^{}", kv.1, kv.0)).collect();
+        print!("Reduced form: ");
+        println!("{} = 0", reduced_terms.join(" + "));
+    }
+
+    fn print_degree(&self) {
+        let d = self.get_degree();
+        println!("Degree: {}", d);
+    }
+
+    fn get_square_coefs(&self) -> (f64, f64, f64) {
+        let a = *self.degrees.get(&2).unwrap_or(&0.);
+        let b = *self.degrees.get(&1).unwrap_or(&0.);
+        let c = *self.degrees.get(&0).unwrap_or(&0.);
+        (a, b, c)
+    }
+
+    fn get_discriminant(&self) -> f64 {
+        let (a, b, c) = self.get_square_coefs();
+        b * b - 4. * a * c
+    }
+
+    fn get_degree(&self) -> i32 {
+        let mut max_degree = 0;
+        for (degree, value) in &self.degrees {
+            if *value != 0. && *degree > max_degree {
+                max_degree = *degree;
+            }
+        }
+        max_degree
+    }
+
+    fn solve_zero_degree(&self) -> String {
+        println!("Solving zero-degree polynomial");
+        let (_, _, c) = self.get_square_coefs();
+        if c == 0. { return "No solution, polynomial is correct".to_owned() }
+        "No solution, polynomial is incorrect".to_owned()
+    }
+
+    fn solve_first_degree(&self) -> String {
+        println!("Solving first-degree polynomial");
+        let (_, b, c) = self.get_square_coefs();
+        (-c / b).to_string()
+    }
+
+    fn solve_second_degree(&self) -> String {
+        println!("Solving second-degree polynomial");
+        let d: f64 = self.get_discriminant();
+        println!("Discriminant: {}", d);
+        match d {
+            d if d < 0. => self.solve_second_degree_neg_d(d),
+            d if d > 0. => self.solve_second_degree_pos_d(d),
+            _ => self.solve_second_degree_zero_d(),
+        }
+    }
+
+    fn solve_second_degree_neg_d(&self, d: f64) -> String {
+        println!("Negative discriminant. Finding complex roots...");
+        "neg d".to_owned()
+    }
+
+    fn solve_second_degree_pos_d(&self, d: f64) -> String {
+        println!("Positive discriminant. Finding real roots...");
+        let (a, b, _) = self.get_square_coefs();
+        let x1: f64 = (-b + d.ft_sqrt()) / 2. / a;
+        let x2: f64 = (-b - d.ft_sqrt()) / 2. / a;
+        x1.to_string() + "," + x2.to_string().as_str()
+    }
+
+    fn solve_second_degree_zero_d(&self) -> String {
+        println!("Zero discriminant. Finding real roots...");
+        "zero d".to_owned()
     }
 }
 
 impl ops::AddAssign<Polynomial> for Polynomial {
     fn add_assign(&mut self, rhs: Polynomial) {
-        self.a += rhs.a;
-        self.b += rhs.b;
-        self.c += rhs.c;
+        for (degree, value) in &rhs.degrees {
+            let deg = self.degrees.entry(*degree).or_insert(0.);
+            *deg += value;
+        }
     }
 }
 
@@ -33,11 +98,12 @@ impl ops::Sub<Polynomial> for Polynomial {
     type Output = Polynomial;
 
     fn sub(self, rhs: Polynomial) -> Self::Output {
-        Polynomial {
-            a: self.a - rhs.a,
-            b: self.b - rhs.b,
-            c: self.c - rhs.c,
+        let mut res= self;
+        for (degree, value) in &rhs.degrees {
+            let deg = res.degrees.entry(*degree).or_insert(0.);
+            *deg -= value;
         }
+        res
     }
 }
 
@@ -64,9 +130,34 @@ impl StringUtils for String {
     }
 }
 
+trait F64Utils {
+    fn ft_sqrt(self) -> f64;
+}
+
+impl F64Utils for f64 {
+    fn ft_sqrt(self) -> f64 {
+        if self <= 0. { return self }
+        let mut sqrt = self / 2.;
+        let mut tmp = 0.;
+        while sqrt != tmp {
+            tmp = sqrt;
+            sqrt = (self / tmp + tmp) / 2.;
+        }
+        sqrt
+    }
+}
+
+fn parse_degree(degree: &String) -> Result<i32, String> {
+    if !degree.starts_with("X^") { return Err("Bad pattern of term: ".to_owned() + degree.as_str()) }
+    match degree.chars().skip(2).collect::<String>().parse::<i32>() {
+        Ok(deg) => Ok(deg),
+        Err(err) => Err(err.to_string()),
+    }
+}
+
 fn parse_term(term: &str) -> Result<Polynomial, String> {
-    println!("Term: {} ", term);
-    let mut poly = Polynomial { a: 0., b: 0., c: 0.};
+    // print!("Term: {} ", term);
+    let mut poly = Polynomial { degrees: HashMap::new() };
     if term.is_empty() { return Ok(poly); }
     if term.starts_with("*") { return Err("Bad pattern of term".to_owned() + term); }
 
@@ -79,39 +170,40 @@ fn parse_term(term: &str) -> Result<Polynomial, String> {
     let mut remain = term.chars().skip_while(|c| c.is_numeric()).collect::<String>();
     if remain.starts_with("*") { remain.remove(0); }
     match remain.as_str() {
-        "X^0" => poly.c += coef,
-        "" => poly.c += coef,
-        "X^1" => poly.b += coef,
-        "X" => poly.b += coef,
-        "X^2" => poly.a += coef,
-        "X*X" => poly.a += coef,
-        _ => return Err("Bad pattern of term: ".to_owned() + term),
-    }
+        "" => poly.degrees.insert(0, coef),
+        "X" => poly.degrees.insert(1, coef),
+        _ => poly.degrees.insert(parse_degree(&remain)?, coef),
+    };
+    // println!("Poly: {:?} ", poly);
     Ok(poly)
 }
 
 fn parse_equation(eq: &str) -> Result<Polynomial, String> {
-    println!("Equation: {}", eq);
+    // println!("Equation: {}", eq);
     if eq.is_empty() { return Err("Empty equation found".to_owned()); }
     let eq: String = String::from("+") + eq;
     let parse_term_cls = |term: &str| parse_term(term
             .chars()
             .take_while(|c| *c != '+' && *c != '-')
             .collect::<String>().as_str());
-    let mut pos_term_res = Polynomial {a: 0., b: 0., c: 0.};
-    let mut neg_term_res = Polynomial {a: 0., b: 0., c: 0.};
+    let mut pos_term_res = Polynomial { degrees: HashMap::new() };
+    let mut neg_term_res = Polynomial { degrees: HashMap::new() };
     for term in eq.split("+").map(parse_term_cls) {
         pos_term_res += term?;
     }
     for term in eq.split("-").map(parse_term_cls) {
         neg_term_res += term?;
     }
+    // println!("Pos terms: {:?}", pos_term_res);
+    // println!("Neg terms: {:?}", neg_term_res);
     Ok(pos_term_res - neg_term_res)
 }
 
-fn parse_polynomial(input: &str) -> Result<Polynomial, String> {
+fn parse_polynomial(input: &String) -> Result<Polynomial, String> {
+    println!("Input: {}", input);
+    let input = input.remove_whitespaces().remove_redundant_operators();
     let equations: Vec<&str> = input.split("=").collect();
-    println!("{:?}", equations);
+    // println!("{:?}", equations);
     match equations.len() {
         1 => Err("Symbol '=' not found".to_owned()),
         2 => Ok(parse_equation(equations[0])?
@@ -120,10 +212,19 @@ fn parse_polynomial(input: &str) -> Result<Polynomial, String> {
     }
 }
 
-fn main() {
-    let input: String = String::from("-5 + 4 * X -+--+-X^2 = X^2");
+fn solve_polynomial(poly: &Polynomial) -> Result<String, String> {
+    poly.print_degree();
+    match poly.get_degree() {
+        0 => Ok(poly.solve_zero_degree()),
+        1 => Ok(poly.solve_first_degree()),
+        2 => Ok(poly.solve_second_degree()),
+        _ => Err("Can be solved only 0, 1, 2 degree polynomials".to_owned()),
+    }
+}
 
-    let input = input.remove_whitespaces().remove_redundant_operators();
+fn main() {
+    let input: String = String::from("-5 + 4 * X -X^2 = 14 * X^2 - 7X - 5");
+
     let poly: Polynomial = match parse_polynomial(&input) {
         Ok(poly) => poly,
         Err(err) => {
@@ -131,6 +232,12 @@ fn main() {
             return;
         },
     };
-
-    println!("{:?}", poly);
+    poly.print_reduced();
+    match solve_polynomial(&poly) {
+        Ok(solution) => println!("Solution: {}", solution),
+        Err(err) => {
+            println!("Solve error: {}", err);
+            return;
+        }
+    }
 }
